151d3d2143eb3c16dccbb2918d36bf91f6b3139f
Brandon G-Home <brandon.gardner2@outlook.com>
2019-02-15 11:29:28 -0600

    Pagination working

--- starwars/src/components/CharacterList/CharacterList.js
+++ starwars/src/components/CharacterList/CharacterList.js
@@ -7,13 +7,61 @@ import Character from "../Character/Character";
 class CharacterList extends React.Component {
   constructor(props) {
     super(props);
+
+    this.state = {
+      charList: [],
+      currentPage: 1,
+      charsPerPage: 3
+    };
   }
+
+  componentDidUpdate = () => {
+    if (this.state.charList !== this.props.charList) {
+      this.hydrateState();
+    }
+  };
+
+  hydrateState = () => {
+    this.setState({
+      charList: this.props.charList
+    });
+  };
+  handleClick = e => {
+    this.setState({
+      currentPage: e.target.id
+    });
+  };
+
   render() {
+    const { charList, currentPage, charsPerPage } = this.state;
+
+    //What characters do we need to display?
+    const indexOfLastChar = currentPage * charsPerPage;
+    const indexOfFirstChar = indexOfLastChar - charsPerPage;
+    const curChars = charList.slice(indexOfFirstChar, indexOfLastChar);
+
+    //Render correct characters
+    const renderChars = curChars.map(character => (
+      <Character char={character} key={character.created} />
+    ));
+
+    //Display page numbers
+    let pageNumbers = [];
+    for (let i = 1; i <= Math.ceil(charList.length / charsPerPage); i++) {
+      pageNumbers.push(i);
+    }
+
+    const renderPageNums = pageNumbers.map(number => {
+      return (
+        <a key={number} id={number} onClick={this.handleClick}>
+          {number}
+        </a>
+      );
+    });
     return (
       <div className="character-list">
-        {this.props.charList.map(character => (
-          <Character char={character} key={character.created} />
-        ))}
+        {renderChars}
+        <div id="page-numbers">{renderPageNums}</div>
       </div>
     );
   }
