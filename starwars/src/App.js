ec758250e9b47d4123053cb82e94bf1119247c5b
Brandon G-Home <brandon.gardner2@outlook.com>
2019-02-15 09:57:05 -0600

    Initial commit

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,5 +1,5 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
 class App extends Component {
   constructor() {
@@ -10,13 +10,13 @@ class App extends Component {
   }
 
   componentDidMount() {
-    this.getCharacters('https://swapi.co/api/people');
+    this.getCharacters("https://swapi.co/api/people");
   }
 
   getCharacters = URL => {
     // feel free to research what this code is doing.
     // At a high level we are calling an API to fetch some starwars data from the open web.
-    // We then take that data and resolve it our state.
+    // We then take that data and resolve it our state
     fetch(URL)
       .then(res => {
         return res.json();
